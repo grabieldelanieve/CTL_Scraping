60daca68a798873be8b9e989cf6f62f95226f737
GrabielDeLaNieve <grabieldelanieve@gmail.com>
2023-06-24 19:59:41 -0400

    Implementacion del HashMap para el guardado de los datos con Keys y sus valores, completado para la primera tabla

--- src/test/java/delanieve/test/CartaNatal.java
+++ src/test/java/delanieve/test/CartaNatal.java
@@ -6,16 +6,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CartaNatal {
+
+
+    ArrayList<String> pla = new ArrayList<String>();
+    ArrayList<String> coordenadas = new ArrayList<String>();
+    ArrayList<String> casas = new ArrayList<String>();
+    ArrayList<String> signos = new ArrayList<String>();
+
+    int counter = 0;
 
     @Test()
     public void test1() throws InterruptedException {
@@ -58,17 +63,14 @@ public class CartaNatal {
 
         jse.executeScript("window.scrollBy(0,2000)", "");
 
-        cartaNatalData(driver);
+        tabla1Value(driver);
+
+        getData();
 
         driver.close();
     }
 
-    @Test
-    public void test2() {
-        System.out.println("Hola Soy el Test #2");
-    }
-
-    public void cartaNatalData(WebDriver driver) {
+    public void tabla1Value(WebDriver driver){
 
         List<String> tablas;
         List<String> imgDomElements;
@@ -78,16 +80,10 @@ public class CartaNatal {
         List<WebElement> rows = driver.findElements(By.xpath("//tr/td/following-sibling::td"));
         List<WebElement> signs = driver.findElements(By.xpath("//tr/td/following-sibling::td/div/img"));
 
-        ArrayList<String> pla = new ArrayList<String>();
-        ArrayList<String> coordenadas = new ArrayList<String>();
-        ArrayList<String> casas = new ArrayList<String>();
-        ArrayList<String> signos = new ArrayList<String>();
-
-        int counter = 0;
-
         planetas = planetsElements.stream().map(s -> s.getText()).collect(Collectors.toList());
         tablas = rows.stream().map(s -> s.getText()).collect(Collectors.toList());
         imgDomElements = signs.stream().map(s -> s.getDomProperty("title")).collect(Collectors.toList());
+
 
         // Planetas
         for (int i = 11; i <= 20; i++) {
@@ -106,10 +102,27 @@ public class CartaNatal {
                 casas.add(tablas.get(i));
             }
         }
+    }
+
+    public void tabla2Value(WebDriver driver) {
+
+        for (int i=73; i<84; i++){
+
+        }
+    }
+
+    public void getData() {
+        Map<String, List<String>> table1 = new HashMap<String, List<String>>();
+        ArrayList<List<String>> g = new ArrayList<>();
+
         // Displaying data from comand line
         while (coordenadas.size() > counter) {
-            System.out.println(pla.get(counter) + " | " + signos.get(counter) + " | " + coordenadas.get(counter) + " | " + casas.get(counter));
+            g.add(Arrays.asList(signos.get(counter), coordenadas.get(counter), casas.get(counter)));
+            table1.put(pla.get(counter), g.get(counter));
             counter++;
+        }
+        for (Map.Entry<String, List<String>> entry : table1.entrySet()){
+            System.out.println(entry.getKey() + " ::: " + entry.getValue());
         }
     }
 }
